4de965f5fe9ab37ff9b4deb9cb8c06a6a07f1f0f
AndrewSpangler <arcticgentoo+public@gmail.com>
2025-07-14 07:55:47 -0700

    move remainder of section macros to wtf templates

--- README.md
+++ README.md
@@ -6,5 +6,4 @@ Use at your own risk
 
 Probably better off using Docke-GE until this is better developed
 
-GPL3 (Including WTF_Script, which will eventually be moved to a separate repo), feel free to steal from this
-
+GPL3
--- src/appsrc/templates/wtf/headers.py
+++ src/appsrc/templates/wtf/headers.py
@@ -3,17 +3,25 @@ Cetadash WTFScript headers
 """
 prefix = "cd"
 
+
 def anchor(id=""): pass
 def bicon(icon=""): pass
 def back_link(content="", url=""): pass
+def container_end(): pass
+def container_start(classes="my-4"): pass
+
+def data_table(
+    name="",
+    columns="",
+    rows="",
+    custom_script='console.log("Table loaded");',
+    for_page=""
+): pass
+def data_table_script(name="", script="", default_rows=25): pass
+def data_table_script_common(): pass
 def icon_heading(content="", icon="", classes="", badge=""): pass
 def script(src=""): pass
 def stylesheet(href=""): pass
-def table_head(items="", id="", classes=""): pass
-def table_row(items="", id="", classes=""): pass
-def table_body(rows="", id="", classes=""): pass
-def table(content="", id="", classes="table table-striped table-hoverable table-sm"): pass 
-
 def section_card_end(id=""): pass
 def section_card_start(id="", label="", classes="my-3"): pass
 def section_card(content="", id="", label="", classes="my-3"): pass
@@ -21,8 +29,23 @@ def section(content="", id="", classes="", label=""): pass
 def section_end(id=""): pass
 def section_start(id="", label=""): pass
 
-def container_end(): pass
-def container_start(classes="my-4"): pass
+def table_button(
+  text="",
+  url_args=("",{}),
+  on_submit=None,
+  btn_type="success",
+  classes=[],
+  float="left",
+  do_action=True,
+  on_click=None,
+  tooltip=None,
+  method="POST"
+): pass
+
+def table_head(items="", id="", classes=""): pass
+def table_row(items="", id="", classes=""): pass
+def table_body(rows="", id="", classes=""): pass
+def table(content="", id="", classes="table table-striped table-hoverable table-sm"): pass 
 
 def labeled_val(val="", key="", kclass="text-muted", vclass=""): pass
 def note(content="", id="", classes=""): pass
@@ -48,3 +71,13 @@ def container_header(
 def header_title(content=""): pass
 def header_value_box(items={}): pass
 def js_confirm(action="", text=""): pass
+
+
+
+
+
+
+# Specialty
+def creator_editor_box(obj=""): pass
+def details_description_card(obj=""): pass
+def edit_logs_card(obj="", log_url_callback=""):pass
